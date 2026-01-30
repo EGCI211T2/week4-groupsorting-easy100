31b45b3940fed09fc7efe5ec079ec8c9ea5fdc8a
tristan druart <118687016+poulpipoz@users.noreply.github.com>
2026-01-30 04:39:15 +0000

    added sort algorithms

--- sorting.h
+++ sorting.h
@@ -21,15 +21,29 @@ void selectionSort(int data[], int length)
 	int i, j, m, mi; 
 	
      display(data,length);
-     
-	
+     mi = 0;
+    for(int j = 0; j < n-1; j++){
+        mi = j;
+        for(i = j+1; i < n; i++){
+            if(a[mi] > a[i]){
+                mi = i;
+            }
+        }
+        swap(a[j], a[mi]);
+        //display(a, n);
+    }
 } 
 
 
 
 void insertion(int a[],int n){
-
-
+  for(int i = n-1; i >= 0; i--){
+          if(a[i]<= newnb){
+              break;
+          }
+          swap(a[i+1], a[i]);
+          //display(a, n);
+      }
 }
 
 void swap(int &a,int &b){
@@ -37,24 +51,21 @@ void swap(int &a,int &b){
   temp=a;
   a=b;
   b=temp;
-  
 }
 
 void bubbleSort(int a[],int n){
 
 int i,j;
 int sorted;
-// how may pair to compare?
+// how many pair to compare?
 for(j=1;j<n;j++){
-
-
-	
+	for(int j = 0; j < i; j++){
+      if(a[i] < a[j]){
+          swap(a[i], a[j]);
+      }
+  }
   display(a,n);
  }
   
 }
 #endif
-
-
-
-
