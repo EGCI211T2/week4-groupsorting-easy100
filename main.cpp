f1fec1c5c5b5af0ab1956b9b099ad1bab759cab0
tristan druart <118687016+poulpipoz@users.noreply.github.com>
2026-01-30 04:54:31 +0000

    pushed non-working version

--- main.cpp
+++ main.cpp
@@ -5,18 +5,24 @@ using namespace std;
 
 int main(int argc, char**argv) {
   int *a,N;
-  /* Make sure you convert 'a' properly*/ 
-  
-
-   display(a,N);
+  cout<<"argc = "<<argc<<endl;
+  if (argc <= 2){
+    cout<<"Not enough arguments"<<endl;
+    return 0;
+  }
+  for(int i = 2; i < argc; i++){
+    cout<<"argv[" << i<<"] : " <<argv[i]<<endl;
+    a[i-2] = atoi(argv[i]);
+  }
+  N = argc - 2;
+  display(a,N);
 
   if(argc>1){
        if(argv[1] =="bubble") bubbleSort(a,N); 
-       else /* Complete the rest*/
-         //insertion(a,N);
+       else if(argv[1] =="insertion") insertion(a,N); 
+       else if(argv[1] =="selection") selectionSort(a,N);
     
-      // selectionSort(a,N);
-       display(a,N);
+       //display(a,N);
       }
  return 0;
 }
--- sorting.h
+++ sorting.h
@@ -1,27 +1,26 @@
-#ifndefine SORTING_H
+#ifndef SORTING_H
 #define SORTING_H
 
 #include <iomanip>
+
+using namespace std;
 /* function prototype*/
 
 
 void display(int a[],int n){
-
     int i;
-
     for(i=0;i<n;i++)
         std::cout<<setw(3)<<a[i];
-
-	std::cout<<std::endl;
+	  std::cout<<std::endl;
   
 }
 
-void selectionSort(int data[], int length) 
+void selectionSort(int a[], int n) 
 { 
-	int i, j, m, mi; 
-	
-     display(data,length);
-     mi = 0;
+	  int i, j, m, mi; 
+    cout<<"Before selection sort: ";
+    display(a,n);
+    mi = 0;
     for(int j = 0; j < n-1; j++){
         mi = j;
         for(i = j+1; i < n; i++){
@@ -32,40 +31,36 @@ void selectionSort(int data[], int length)
         swap(a[j], a[mi]);
         //display(a, n);
     }
+    cout<<"After selection sort: ";
+    display(a,n);
 } 
 
 
 
 void insertion(int a[],int n){
-  for(int i = n-1; i >= 0; i--){
-          if(a[i]<= newnb){
-              break;
-          }
-          swap(a[i+1], a[i]);
-          //display(a, n);
-      }
+  
 }
 
 void swap(int &a,int &b){
- int temp;
-  temp=a;
-  a=b;
-  b=temp;
+    int temp;
+    temp=a;
+    a=b;
+    b=temp;
 }
 
 void bubbleSort(int a[],int n){
-
-int i,j;
-int sorted;
-// how many pair to compare?
-for(j=1;j<n;j++){
-	for(int j = 0; j < i; j++){
-      if(a[i] < a[j]){
-          swap(a[i], a[j]);
+    cout<<"Before bubble sort: ";
+    int i,j;
+    int sorted;
+    // how many pair to compare?
+    for(j=1;j<n;j++){
+    	for(int j = 0; j < i; j++){
+          if(a[i] < a[j]){
+              swap(a[i], a[j]);
+          }
       }
-  }
-  display(a,n);
- }
-  
+      //display(a,n);
+    }
+    cout<<"After bubble sort: ";
 }
 #endif
